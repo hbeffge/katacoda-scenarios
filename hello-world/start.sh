0a5e8c0bbe93b746ac56bb3af2175dfa15a0dd4e
hbeffge <65234543+hbeffge@users.noreply.github.com>
2020-06-30 09:50:58 +0200

    Update start.sh

--- hello-world/start.sh
+++ hello-world/start.sh
@@ -1,7 +1,5 @@
-launch.sh
-
-#head -8 /opt/launch-kubeadm.sh > /root/launch-kubeadm.sh
-#cp /opt/katacoda-cloud-provider.yaml /root/katacoda-cloud-provider.yaml
-#chmod +x /root/launch-kubeadm.sh
-#/root/launch-kubeadm.sh
-#kubectl apply -f /root/weave.yaml
+head -8 /opt/launch-kubeadm.sh > /root/launch-kubeadm.sh
+cp /opt/katacoda-cloud-provider.yaml /root/katacoda-cloud-provider.yaml
+chmod +x /root/launch-kubeadm.sh
+/root/launch-kubeadm.sh
+kubectl apply -f /root/weave.yaml
